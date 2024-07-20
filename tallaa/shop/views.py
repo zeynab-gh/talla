103e60afdf991df8d2f26ae96a15adc1bb61c6d5
zeynab <computer.1818@yahoo.com>
2024-07-20 17:11:06 +0430

    price

--- tallaa/shop/views.py
+++ tallaa/shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from .models import Product
 
 def helloworld(request):
-    return render(request, 'index.html')
+
+    all_products=Product.objects.all()
+    
+    return render(request, 'index.html', {'products':all_products})
