05626f1ad0566f6906015f9164acdb21d4289050
zeynab <computer.1818@yahoo.com>
2024-07-20 21:06:53 +0430

    price

--- tallaa/shop/models.py
+++ tallaa/shop/models.py
@@ -25,6 +25,8 @@ class Product(models.Model):
     price =models.DecimalField(default=0,max_digits=50,decimal_places=0)
     category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     picture=models.ImageField(upload_to='upload/products/')
+    is_sale=models.BooleanField(default=False)
+    sale_price=models.DecimalField(default=0,max_digits=50,decimal_places=0)
 
     def __str__(self):
         return  self.name 
