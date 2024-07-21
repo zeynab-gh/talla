c084be58ade3e08f25e700823e60c5c9e3b99e5d
zeynab <computer.1818@yahoo.com>
2024-07-21 10:27:42 +0430

    login

--- tallaa/shop/urls.py
+++ tallaa/shop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
-from .views import helloworld,about
+from .views import helloworld,about,login_user,logout_user
 
 
 urlpatterns = [
   
      path('',helloworld,name="home"),
-     path('about/',about,name="about")
+     path('about/',about,name="about"),
+     path('login/',login_user,name="login"),
+     path('logout/',logout_user,name="logout"),
 ]
--- tallaa/shop/views.py
+++ tallaa/shop/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .models import Product
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages   
+
+
 
 def helloworld(request):
     all_products=Product.objects.all()
@@ -8,3 +12,29 @@ def helloworld(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def   login_user(request):
+   if request.method == 'POST':
+       username=request.POST.get('username', '')
+       password=request.POST.get('password','')
+       
+
+       user=authenticate(request, username=username, password=password)
+       if user is not None:
+          login(request, user)
+          messages.success(request,('با موفقیت وارد شدید'))
+          return redirect('home')
+       else:
+           messages.success(request,('مشکلی در ورود پیش امده'))
+           return redirect('login')
+    
+   else:
+  
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,("با موفقیت خارج شدید"))
+    return redirect ("home")
